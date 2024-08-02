683222b25d7352419ed819dde00df6f84115aa3f
veridelisi <72710290+veridelisi@users.noreply.github.com>
2024-08-03 00:01:37 +0300

    Update senaryo17.py

--- senaryo17.py
+++ senaryo17.py
@@ -7,60 +7,87 @@ i)TCMB işlemin ilk bacağında (haberde valör diye geçiyor) bankalara döviz 
 ii)Ancak bu işlemin bir de ikinci bacağı yani vade bacağı var. Bu bacağı da bilanço dışında yani nazım hesaplarda izliyoruz. Vade geldiğinde TCMB ilk bacak işlemi sonucu çektiği TL’yi geri verecek ve karşılığında verdiği dövizi geri alacak; elbette bankada bu ikinci bacak işleminde TL alacak ve döviz verecek. Lakin önemli bir husus var göz önüne alınması gereken: Bu ikinci bacak işlemi sonucunda ortaya çıkacak rezerv para miktarı ilk bacak işlemi sonucundan çekilen rezerv para miktarından daha yüksek olacak. Bu durumun nedeni, 
 TCMB’nin ilk bacak işlemi ile sterilize ettiği TL’ye faiz ödeyecek olması. Üstelik bu faiz oranı, kendi politika faizi olan %50 “etrafında” oluşacak
 """
+pip install abcFinance
+from abcFinance import Ledger
+from IPython.core.display import SVG
+from IPython.display import display_svg
 
 from abcFinance import Ledger
 from IPython.core.display import SVG
 from IPython.display import display_svg
 
-
     
 #  Her aktörün bilançosunun artık hesabı,(Varlık-Yükümlülük),Sermaye olarak adlandırılıyor.
 Xbankasi = Ledger(residual_account_name="Sermaye")
 Merkez_Bankasi = Ledger(residual_account_name="Sermaye")
+XbankasiNazim = Ledger(residual_account_name="Sermaye")
+Merkez_BankasiNazim = Ledger(residual_account_name="Sermaye")
 
 
 #  Bankaların varlık ve yükümlülük kalemleri tanımlanıyor.
-Xbankasi.make_asset_accounts(['Döviz','Tahvil', 'Döviz Alacak','Rezerv Alacak','Rezerv'])
-Xbankasi.make_liability_accounts(['Döviz Borç','Rezerv Borç'])
+Xbankasi.make_asset_accounts(['Döviz','Tahvil', 'Rezerv'])
+Xbankasi.make_liability_accounts(['Mevduat'])
+XbankasiNazim.make_asset_accounts(['Döviz Alacak','Rezerv Alacak'])
+XbankasiNazim.make_liability_accounts(['Döviz Borç','Rezerv Borç'])
 
 
 # Merkez Bankasının varlık ve yükümlülük kalemleri tanımlanıyor.
-Merkez_Bankasi.make_asset_accounts(['Döviz','Döviz Alacak','Rezerv Alacak'])
-Merkez_Bankasi.make_liability_accounts(['Döviz Borç','Rezerv Borç','Rezerv'])
-
+Merkez_Bankasi.make_asset_accounts(['Döviz'])
+Merkez_Bankasi.make_liability_accounts(['Rezerv'])
+Merkez_BankasiNazim.make_asset_accounts(['Döviz Alacak','Rezerv Alacak'])
+Merkez_BankasiNazim.make_liability_accounts(['Döviz Borç','Rezerv Borç'])
 
 #Başlangıç
 Xbankasi.book(debit=[('Rezerv',100)],credit=[('Sermaye',100)])
 Merkez_Bankasi.book(debit=[('Döviz',100)],credit=[('Rezerv',100)])
 
 #Başlangıç- Bilançolar
-display_svg(SVG(Xbankasi.draw_balance_sheet("Banka X", width=500)))
-display_svg(SVG(Merkez_Bankasi.draw_balance_sheet("Merkez Bankası", width=500)))
+display_svg(SVG(Xbankasi.draw_balance_sheet("Banka X Bilanço", width=500)))
+display_svg(SVG(Merkez_Bankasi.draw_balance_sheet("Merkez Bankası Bilanço", width=500)))
 
 
-#Swap
-Xbankasi.book(debit=[('Döviz',100)],credit=[('Rezerv',100)])
-Merkez_Bankasi.book(debit=[('Rezerv',100)],credit=[('Döviz',100)])
+
+
+
+
+# 1 dolar= 1 TL (Yiğit BULUT)
+# TCMB işlemin ilk bacağında bankalara 60 dolar döviz verecek bankalarda 60 TL rezerv ile ödeyecek
+# TCMB bilançosunda hem aktif hem pasif azalırken
+# Banka bilançosunda aktifte döviz artarken rezerv azalacak
+# Nazım da TCMB döviz alacağı ve rezerv borcu artarken
+# Nazım da bankanın Rezerv alacağı ve Döviz borcu artacaktır.
+
+Xbankasi.book(debit=[('Döviz',60)],credit=[('Rezerv',60)])
+Merkez_Bankasi.book(debit=[('Rezerv',60)],credit=[('Döviz',60)])
+XbankasiNazim.book(debit=[('Rezerv Alacak',60)],credit=[('Döviz Borç',60)])
+Merkez_BankasiNazim.book(debit=[('Döviz Alacak',60)],credit=[('Rezerv Borç',60)])
 
 #Başlangıç- Bilançolar
-display_svg(SVG(Xbankasi.draw_balance_sheet("Banka X", width=500)))
-display_svg(SVG(Merkez_Bankasi.draw_balance_sheet("Merkez Bankası", width=500)))
+display_svg(SVG(Xbankasi.draw_balance_sheet("Banka X Bilanço", width=500)))
+display_svg(SVG(XbankasiNazim.draw_balance_sheet("Banka X Nazım", width=500)))
+display_svg(SVG(Merkez_Bankasi.draw_balance_sheet("Merkez Bankası Bilanço", width=500)))
+display_svg(SVG(Merkez_BankasiNazim.draw_balance_sheet("Merkez Bankası Nazım", width=500)))
 
 
-#Swap Nazım
-Xbankasi.book(debit=[('Rezerv Alacak',100)],credit=[('Döviz Borç',100)])
-Merkez_Bankasi.book(debit=[('Döviz Alacak',100)],credit=[('Rezerv Borç',100)])
+
+
+
+
+# 1 dolar= 1 TL (Yiğit BULUT)
+# TCMB işlemin ilk bacağında bankalara 60 dolar döviz geri alacak bankalara 60 TL rezerv ile ödeyecek
+# TCMB bilançosunda hem aktif hem pasif artarken
+# Banka bilançosunda aktifte rezerv artarken döviz azalacak
+# Nazım da TCMB döviz alacağı ve rezerv borcu azalırken
+# Nazım da bankanın Rezerv alacağı ve Döviz borcu azalacaktır
+
+Xbankasi.book(debit=[('Rezerv',60)],credit=[('Döviz',60)])
+Merkez_Bankasi.book(debit=[('Döviz',60)],credit=[('Rezerv',60)])
+XbankasiNazim.book(debit=[('Döviz Borç',60)],credit=[('Rezerv Alacak',60)])
+Merkez_BankasiNazim.book(debit=[('Rezerv Borç',60)],credit=[('Döviz Alacak',60)])
 
 #Başlangıç- Bilançolar
-display_svg(SVG(Xbankasi.draw_balance_sheet("Banka X", width=500)))
-display_svg(SVG(Merkez_Bankasi.draw_balance_sheet("Merkez Bankası", width=500)))
-
-
-
-"""
-Şekil 1’in son iki sütununda ise özellikle 2023 Mayıs seçimleri öncesi yoğun bir şekilde kullanılmış önce bir spot satım ve sonra alım yönlü bir döviz swapından oluşan ve bir sentetik forward yaratan eski swap işlemlerinin kaydı sunuluyor:
-i)Spot bacakta TCMB kurun yükselmesini engellemek için yerel bankaya döviz satar, karşılığında rezerv para yok eder3.
-ii)TCMB spot bacakta sattığı dövizi, alım yönlü döviz swapının valör bacağında geri alıp, spot bacakta aldığı TL’yi geri verir. Böylece TCMB ve banka bilançoları başlangıç durumuna döner. Dolayısıyla TCMB brüt rezervi değişmez
-iii)Ortaya çıkan sentetik forwardın vade bacağında ise TCMB swapın valör bacağında aldığı döviz için faiz öderken verdiği TL (rezerv para) için faiz alır. Bu faiz de, yine o dönemki kendi politika faizi olan %8.5 “etrafında” oluşur
-
-"""
+display_svg(SVG(Xbankasi.draw_balance_sheet("Banka X Bilanço", width=500)))
+display_svg(SVG(Merkez_Bankasi.draw_balance_sheet("Merkez Bankası Bilanço", width=500)))
+print("......................................................................")
+display_svg(SVG(XbankasiNazim.draw_balance_sheet("Banka X Nazım Temizlendi", width=500)))
+display_svg(SVG(Merkez_BankasiNazim.draw_balance_sheet("Merkez Bankası Nazım Temizlendi", width=500)))
